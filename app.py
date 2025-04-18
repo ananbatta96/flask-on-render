adf84645eaa1777c6cea100468b837f693fb4527
ananbatta96 <35363552+ananbatta96@users.noreply.github.com>
2025-04-18 16:36:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello from Mohanad on Render!</h1>"
+    return "<h1>Hello from khawla on Render!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
